cfbf0405a782e6b3e771ef287f935c7d39e8aae8
BRian <cimminob@gmail.com>
2021-02-22 09:58:40 -0500

    ADD PAGING

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { StoreModule } from './store/store/store.module';
+import { StoreModule } from './store/store.module';
 
 @NgModule({
     imports: [
